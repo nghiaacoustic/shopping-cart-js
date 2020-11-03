e696e3807cd0d4352ed71be3d9fd921efa369e0c
nghiatic <pnghiaacoustic@gmail.com>
2020-11-03 20:33:04 +0700

    fix tenSP

--- src/shoppingCart/san-pham.js
+++ src/shoppingCart/san-pham.js
@@ -8,7 +8,7 @@ export default class SanPham extends Component {
         <div className="card">
           <img className="card-img-top" src={this.props.product.hinhAnh} alt="" />
           <div className="card-body">
-            <h4 className="card-title">VinSmart Live</h4>
+            <h4 className="card-title">{this.props.product.tenSP}</h4>
             <button className="btn btn-success" onClick={()=> {this.props.bidingDetails(this.props.product)}}>Chi tiết</button>
             <button className="btn btn-danger" onClick={()=>{this.props.addCart(this.props.product)}}>Thêm giỏ hàng</button>
           </div>
